0f6bfcc4d64a8c0a4c77495a90f29112ad996f08
plugxr-android <surya.nadiminti@itsinnovare.com>
2020-02-20 13:24:33 +0530

    committed

---
+++ unityLibrary/src/main/java/com/unity3d/player/customfonts/MyButton.java
@@ -0,0 +1,35 @@
+package com.unity3d.player.customfonts;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+/**
+ * Created by one on 3/12/15.
+ */
+public class MyButton extends Button {
+
+    public MyButton(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public MyButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public MyButton(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        if (!isInEditMode()) {
+            Typeface tf = Typeface.createFromAsset(getContext().getAssets(), "fonts/axima_regular.ttf");
+            setTypeface(tf);
+        }
+    }
+
+}
